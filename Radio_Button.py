5dc37c1b153fb652e19e19951157db4027c7504e
Ameer Hamza <ameer.hamza.div@gmail.com>
2025-01-09 01:36:44 +0500

    create radio button

--- Radio_Button.py
+++ Radio_Button.py
@@ -5,5 +5,19 @@ root = Tk()
 root.geometry('600x400')
 root.title('Radio Button')
 
+def gender():
+    if val.get() == 1:
+        print('You are a Male')
+    elif val.get() == 2:
+        print('You are a Female')
+Label(root, text='Gender', font='robots 15 bold').pack()
+
+val = IntVar()
+
+radio = Radiobutton(root, text='Male', variable=val, value=1).pack()
+radio = Radiobutton(root, text='Female', variable=val, value=2).pack()
+
+Button(root, text='Submit', command=gender).pack()
+
 
 root.mainloop()
